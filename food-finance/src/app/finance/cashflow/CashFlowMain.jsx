36250219ef84b3820689420b735a7408a4253e04
julio <jandrade.pereira1@gmail.com>
2024-07-19 21:03:20 -0300

    Added DateInput in CashFlowMain component

--- food-finance/src/app/finance/cashflow/CashFlowMain.jsx
+++ food-finance/src/app/finance/cashflow/CashFlowMain.jsx
@@ -2,17 +2,21 @@
 import React, { useEffect, useState, useRef } from "react"
 import Table from "../../components/table/Table"
 import SearchInput from "@/app/components/inputs/searchInput/SearchInput";
-import Dropdown from "@/app/components/dropdowns/Dropdown";
+import DateDropdown from "@/app/components/dropdowns/DateDropdown";
 import { files, months } from "@/mockData";
-import Calendar from "@/app/components/calendar/Calendar";
+import DateInput from "@/app/components/inputs/searchInput/DateInput";
 
 function CashFlowMain({ data }) {
     const [showCalendar, setShowCalendar] = useState(false);
     const calendarRef = useRef(null);
     const dropdownRef = useRef(null);
 
-    const handleShowCalendar = () => {
+    const onShowCalendar = () => {
         setShowCalendar(true);
+    }
+
+    const onHideCalendar = () => {
+        setShowCalendar(false);
     }
 
     const handleClickOutside = (event) => {
@@ -33,19 +37,20 @@ function CashFlowMain({ data }) {
         <section className="lg:pl-4 my-5 z-10">
             <div className="flex flex-col border px-5 py-5 mx-5 bg-white shadow-md sm:rounded-lg min-h-screen">
                 <div className="flex items-center justify-between mb-5">
+                    <div className="flex justify-between relative">
+                        <div ref={dropdownRef}>
+                            <DateDropdown data={months}  onShowCalendar={onShowCalendar} onHideCalendar={onHideCalendar}/>
+
+                            {showCalendar && (
+                                <div className="absolute flex flex-row z-30 my-2 py-2 bg-white border rounded-lg shadow-xl" ref={calendarRef}>
+                                    <DateInput />
+                                    <DateInput />
+                                </div>
+                            )}
+                        </div>
+                    </div>
                     <div>
                         <SearchInput />
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <Dropdown data={files} title={"Exportar"}/>
-                        <div ref={dropdownRef}>
-                            <Dropdown data={months} onShowCalendar={handleShowCalendar}/>
-                        </div>
-                        {showCalendar && (
-                            <div ref={calendarRef}>
-                                <Calendar />
-                            </div>
-                        )}
                     </div>
                 </div>
                 <Table data={data} />
